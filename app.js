36b5681c5d72c9e40b57e934dc11d5484e15dfc0
FrontElvin <rustamove629@gmail.com>
2024-04-20 12:08:22 +0300

    Добавил парсинг и миддлвары

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-     // app.js 
-     const express = require('express');
-     
-     // Импортируем роутеры
-     const mainRoute = require('./routes/main');
-     const gamesRouter = require('./routes/games'); 
-     
-     const PORT = 3000;
-     const app = express();
-     
-     // Запускаем
-     app.use(mainRoute, gamesRouter); 
-     
-     app.listen(PORT, () => {
-       console.log(`Server is running at PORT http://localhost:${PORT}`);
-     })
-    
-     // Теперь клиент имеет доступ только к публичным файлам
-     // app.use(express.static(path.join(__dirname, 'public'))); 
+// app.js 
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mainRoute = require('./routes/main');
+const gamesRouter = require('./routes/games'); 
+
+const PORT = 3000;
+const app = express();
+
+app.use(
+    bodyParser.json(),
+    express.static(path.join(__dirname, 'public')),
+    mainRoute,
+    gamesRouter
+); 
+
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT http://localhost:${PORT}`);
+})
      
--- routes/games.js
+++ routes/games.js
@@ -1,10 +1,11 @@
      // routes/games.js
      const gamesRouter = require("express").Router(); // Создали роутер
-     const { getAllGames, deleteGame } = require("../controllers/games")
+     const { getAllGames, deleteGame, addGameController } = require("../controllers/games")
      
      
      gamesRouter.get("/games", getAllGames)
      gamesRouter.delete("/games/:id", deleteGame);
-     
+     gamesRouter.post("/games", addGameController);
+
      module.exports = gamesRouter;
      
